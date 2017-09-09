9468e3e5576a9c90a852e062f732ddcb7d860469
Dan Mayor <dmayor@digivance.com>
2017-09-09 19:51:15 -0400

    Some suggestions to the gui get methods, would recomend setting environment GOPATH variable and referencing packages like github.com/You/project/package. Unable to run the app, error on line 193 of gui.go :(

--- alerts/tresholdRequestHTTP.go
+++ alerts/tresholdRequestHTTP.go
@@ -1,19 +1,20 @@
 package alerts
 
 import (
-    "time"
-    "fmt"
-    "http-monitoring/monitoring"
+	"fmt"
+	"time"
+
+	"github.com/DanMayor/http-monitoring/monitoring"
 )
 
 // Number max of HTTP requests before raising an alert, between 2 AlertThresholdHTTP() calls
 const (
-    RequestsThreshold uint64 = 25
+	RequestsThreshold uint64 = 25
 )
 
 var (
-    alertTraffic     = "[ %s : High traffic generated an alert - hits = %d, threshold = %d](fg-white,bg-red)"
-    recoveredTraffic = "[ %s : High traffic recovered](fg-white,bg-green)"
+	alertTraffic     = "[ %s : High traffic generated an alert - hits = %d, threshold = %d](fg-white,bg-red)"
+	recoveredTraffic = "[ %s : High traffic recovered](fg-white,bg-green)"
 )
 
 var IsAlertState = false
@@ -25,24 +26,24 @@ var HighTrafficAlert_chan = make(chan string, 1)
 var previousTotalRequests uint64 = 0
 
 /*
-    Function supposed to be called each X seconds, sending alert/recovery messages if number of requests
-    received during its last 2 calls is too high/low
-    (send nothing if IsAlertState == false and lastNumberRequests < RequestsThreshold)
- */
+   Function supposed to be called each X seconds, sending alert/recovery messages if number of requests
+   received during its last 2 calls is too high/low
+   (send nothing if IsAlertState == false and lastNumberRequests < RequestsThreshold)
+*/
 func AlertThresholdHTTP() {
-    monitoring.MonitoringDataStruct.Mux.Lock()
-    defer monitoring.MonitoringDataStruct.Mux.Unlock()
+	monitoring.MonitoringDataStruct.Mux.Lock()
+	defer monitoring.MonitoringDataStruct.Mux.Unlock()
 
-    lastNumberRequests := monitoring.MonitoringDataStruct.TotalRequests - previousTotalRequests
+	lastNumberRequests := monitoring.MonitoringDataStruct.TotalRequests - previousTotalRequests
 
-    if lastNumberRequests >= RequestsThreshold {
-        IsAlertState = true
-        HighTrafficAlert_chan <- fmt.Sprintf(alertTraffic, time.Now().String(), lastNumberRequests, RequestsThreshold)
+	if lastNumberRequests >= RequestsThreshold {
+		IsAlertState = true
+		HighTrafficAlert_chan <- fmt.Sprintf(alertTraffic, time.Now().String(), lastNumberRequests, RequestsThreshold)
 
-    } else if IsAlertState && lastNumberRequests < RequestsThreshold {
-        IsAlertState = false
-        HighTrafficAlert_chan <- fmt.Sprintf(recoveredTraffic, time.Now().String())
-    }
+	} else if IsAlertState && lastNumberRequests < RequestsThreshold {
+		IsAlertState = false
+		HighTrafficAlert_chan <- fmt.Sprintf(recoveredTraffic, time.Now().String())
+	}
 
-    previousTotalRequests = monitoring.MonitoringDataStruct.TotalRequests
+	previousTotalRequests = monitoring.MonitoringDataStruct.TotalRequests
 }
--- monitoring/monitoring.go
+++ monitoring/monitoring.go
@@ -1,30 +1,30 @@
 package monitoring
 
 import (
-    "time"
-    "sync"
-    "strconv"
-    "log"
+	"log"
+	"strconv"
+	"sync"
+	"time"
 )
 
 /*
-    Structure representing the metrics to be monitored
-    Used between many goroutines, so don't forget the mutex (Mux)
+   Structure representing the metrics to be monitored
+   Used between many goroutines, so don't forget the mutex (Mux)
 */
 type monitoringData struct {
-    TotalSize     uint64
-    TotalRequests uint64
-    MapURLsection map[string]int
-    MapHTTPstatus map[string]int
-    Mux           sync.Mutex
+	TotalSize     uint64
+	TotalRequests uint64
+	MapURLsection map[string]int
+	MapHTTPstatus map[string]int
+	Mux           sync.Mutex
 }
 
 // Initialisation of the monitoringData struct.
 var MonitoringDataStruct = monitoringData{
-    TotalSize:     0,
-    TotalRequests: 0,
-    MapURLsection: make(map[string]int),
-    MapHTTPstatus: make(map[string]int),
+	TotalSize:     0,
+	TotalRequests: 0,
+	MapURLsection: make(map[string]int),
+	MapHTTPstatus: make(map[string]int),
 }
 
 // Channel used to send the MonitoringDataStruct to the UI
@@ -32,42 +32,42 @@ var MonitoringData_chan = make(chan monitoringData, 1)
 
 // We can add many functions with the same trigger time, i.e: Monitor( 10, f1, f2, f3...)
 func Monitor(seconds uint32, fs ...func()) {
-    duration := time.Duration(seconds) * time.Second
-    for {
-        time.Sleep(duration)
-        for _, f := range fs {
-            go f()
-        }
-    }
+	duration := time.Duration(seconds) * time.Second
+	for {
+		time.Sleep(duration)
+		for _, f := range fs {
+			go f()
+		}
+	}
 }
 
 func UpdateMonitoringDataStruct(s LineLogStruct) {
-    MonitoringDataStruct.Mux.Lock()
-    defer MonitoringDataStruct.Mux.Unlock()
+	MonitoringDataStruct.Mux.Lock()
+	defer MonitoringDataStruct.Mux.Unlock()
 
-    section := GetURLSection(s)
+	section := GetURLSection(s)
 
-    // URL section counter (i.e : /pages : 4 )
-    MonitoringDataStruct.MapURLsection[section] += 1
+	// URL section counter (i.e : /pages : 4 )
+	MonitoringDataStruct.MapURLsection[section] += 1
 
-    // HTTP status counter (i.e : 200, 404, 500...)
-    MonitoringDataStruct.MapHTTPstatus[s.Status] += 1
+	// HTTP status counter (i.e : 200, 404, 500...)
+	MonitoringDataStruct.MapHTTPstatus[s.Status] += 1
 
-    // Increase counter of HTTP requests
-    MonitoringDataStruct.TotalRequests += 1
+	// Increase counter of HTTP requests
+	MonitoringDataStruct.TotalRequests += 1
 
-    size, err := strconv.ParseUint(s.Size, 10, 64)
-    if err != nil {
-        log.Println(err)
-        size = 0
-    }
+	size, err := strconv.ParseUint(s.Size, 10, 64)
+	if err != nil {
+		log.Println(err)
+		size = 0
+	}
 
-    MonitoringDataStruct.TotalSize += size
+	MonitoringDataStruct.TotalSize += size
 }
 
 func SendMonitoringDataToUI() {
-    MonitoringDataStruct.Mux.Lock()
-    defer MonitoringDataStruct.Mux.Unlock()
+	MonitoringDataStruct.Mux.Lock()
+	defer MonitoringDataStruct.Mux.Unlock()
 
-    MonitoringData_chan <- MonitoringDataStruct
+	MonitoringData_chan <- MonitoringDataStruct
 }
